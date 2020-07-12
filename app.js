75ab929f1b71a30199a12baf665ff00db253539a
VINIT <vinitkumar7070@gmail.com>
2020-07-12 13:05:44 -0700

    1st commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const app = express();
+
+const errorController = require('./controllers/errorController');
+const User = require('./models/user');
+const MONGODB_URI = 'mongodb://localhost:27017/shop';
+
+const store = MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+})
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }))
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+  .then(result => {
+    console.log('Connected!')
+    User.findOne()
+      .then(user => {
+        if (!user) {
+          const user = new User({
+            name: 'Vinit',
+            email: 'vinit@gmail.com',
+            cart: {
+              items: []
+            }
+          });
+          user.save();
+        }
+      })
+    app.listen(3000, () => console.log('Listening on 3000'));
+  })
+  .catch(err => console.log("Mongoose Error", err.message));
---
+++ controllers/authController.js
@@ -0,0 +1,27 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: false
+    });
+};
+
+exports.postLogin = (req, res, next) => {
+    User.findById('5f0a683b94f6e23014632dcd')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save(err => {
+                res.redirect('/');
+            });
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        res.redirect('/');
+    });
+};
